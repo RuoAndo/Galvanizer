e05a4c0e47b59cf28422a7dad1cc568e5423c209
Ruo Ando <ando.ruo@gmail.com>
2019-01-22 20:04:00 +0900

    auto

--- 2019.01/auto.sh
+++ 2019.01/auto.sh
@@ -1,5 +1,6 @@
 time ./listup.pl > list
 time ./global-t.sh list > flist
+tim3 python 2.py flist > t.csv
 time python 1.py flist > flist2
 time ./global-rx.sh flist2 > flist3
-time python 2.py flist3 > flist4
+time python 2.py flist3 > r.csv
