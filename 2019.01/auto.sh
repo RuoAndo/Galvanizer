1056412374b0d0ba9cda8f3e9ad3522b35c9e043
Ruo Ando <ando.ruo@gmail.com>
2019-07-06 19:05:49 +0900

    auto

--- 2019.01/auto.sh
+++ 2019.01/auto.sh
@@ -2,7 +2,7 @@ apt install -y global
 gtags -v
 time ./listup.pl > list
 time ./global-t.sh list > flist
-tim3 python 2.py flist > t.csv
+time python 2.py flist > t.csv
 time python 1.py flist > flist2
 time ./global-rx.sh flist2 > flist3
 time python 2.py flist3 > r.csv
